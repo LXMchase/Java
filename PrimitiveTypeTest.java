45df28c95f7a5bce0a0aa1f7c804b6ecc7d5b2ec
LXMchase <257088928@qq.com>
2019-03-09 14:30:55 +0800

    输出几种数据类型的范围和二进制位数
    Signed-off-by: LXMchase <257088928@qq.com>

---
+++ PrimitiveTypeTest.java
@@ -0,0 +1,23 @@
+public class PrimitiveTypeTest{
+	public static void main(String[] args){
+		//byte
+		System.out.println("基本类型 byte 二进制位数:"+Byte.SIZE);
+		System.out.println("封装类：java.lang.Byte");
+		System.out.println("最小值：Byte.MIN_VALUE="+Byte.MIN_VALUE);
+		System.out.println("最大值：Byte.MAX_VALUE="+Byte.MAX_VALUE);
+		System.out.println();
+		//short
+		System.out.println("基本类型 short 二进制位数："+Short.SIZE);
+		System.out.println("封装类： java.lang.Short");
+		System.out.println("最小值：Short.MIN_VALUE"+Short.MIN_VALUE);
+		System.out.println("最大值：Short.MAX_VALUE"+Short.MAX_VALUE);
+		System.out.println();
+		//char
+		System.out.println("基本类型 char 二进制位数："+Character.SIZE);
+		System.out.println("封装类：java.lang.Character");		
+		System.out.println("最小值：Character.MIN_VALUE"+Character.MIN_VALUE);
+		System.out.println("最大值：Character.MAX_VALUE"+Character.MAX_VALUE);
+		System.out.println();
+	}
+}
+		
